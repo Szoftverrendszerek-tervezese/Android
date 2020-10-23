47c10b04228682a61cef072034b8e53692f1cdda
annamariafodor <annamariafodor98@gmail.com>
2020-10-23 11:46:33 +0300

    initialize develop branch

---
+++ app/src/main/java/com/example/application/authentication/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.application.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.application.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
