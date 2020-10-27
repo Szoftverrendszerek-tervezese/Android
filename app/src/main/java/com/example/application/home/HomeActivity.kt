798b6b79b3451964eb9b130c073f1c1fabde047a
Horvath Aron <haronka99@gmail.com>
2020-10-27 13:23:22 +0200

    layput need improvement

--- app/src/main/java/com/example/application/home/HomeActivity.kt
+++ app/src/main/java/com/example/application/home/HomeActivity.kt
@@ -2,11 +2,13 @@ package com.example.application.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.application.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Helo", "Home");
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.fragment_register)
     }
 }
