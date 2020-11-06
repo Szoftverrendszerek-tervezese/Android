e4a4397d22fadc740d87b466c7647e8d3d59acad
Horvath Aron <haronka99@gmail.com>
2020-11-06 19:27:54 +0200

    initalize again - testing

--- app/src/main/java/com/example/application/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/example/application/authentication/AuthenticationActivity.kt
@@ -2,11 +2,13 @@ package com.example.application.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.application.R
 
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Helo", "authentication")
         setContentView(R.layout.activity_authentication)
     }
 }
--- app/src/main/java/com/example/application/home/HomeActivity.kt
+++ app/src/main/java/com/example/application/home/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.example.application.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.application.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_register)
+        Log.d("Helo", "homeactivity")
+        setContentView(R.layout.activity_home)
+        Log.d("Helo", "after fragment register")
     }
 }
