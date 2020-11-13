fdb6bd2a2f3302f2f89cfb8d04949d676347baee
Horvath Aron <haronka99@gmail.com>
2020-11-13 22:00:18 +0200

    I need to find out how to solve the logged in user problem

--- app/src/main/java/com/example/application/splash/SplashActivity.kt
+++ app/src/main/java/com/example/application/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.application.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.application.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +15,6 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
     }
+
+
 }
