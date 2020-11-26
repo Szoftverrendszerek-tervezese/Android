2d1c6319405857d248153f577eedc09140a9e164
Horvath Aron <haronka99@gmail.com>
2020-11-26 10:09:06 +0200

    botom nav corrected

--- app/src/main/java/com/example/application/home/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/application/home/fragments/AccountFragment.kt
@@ -15,7 +15,7 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
