9aa8b5b244ebe6c8ee1089ff32a57cf49f7e241e
Horvath Aron <haronka99@gmail.com>
2020-12-06 13:52:02 +0200

    search done

--- app/src/main/java/com/example/application/home/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/application/home/fragments/SearchFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -25,6 +26,7 @@ class SearchFragment : Fragment() {
     private val viewModel: GeneralViewModel by activityViewModels()
     private lateinit var articles: MutableList<RecyclerItem>
     private var articleTitles: MutableList<String> = mutableListOf()
+    private var articlePairs: MutableList<Pair<String, String>> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,30 +41,44 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         for (i in 0 until articles.size) {
             var newString = articles[i].title + " by " + articles[i].author
-            Log.d("Helo", "newstring : $newString")
+            val pair = Pair(articles[i].title, articles[i].author)
+            Log.d("Helo", "newstringre : $newString")
+            articlePairs.add(i, pair)
             articleTitles.plusAssign(newString)
         }
 
         Log.d("Helo", "size: ${articles.size}")
+        Log.d("Helo", "articlePairs: $articlePairs")
 
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, articleTitles)
+        val adapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, articleTitles)
         binding.autoComplete.setAdapter(adapter)
-
 
         binding.autoComplete.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id -> //here the article need to be shown
-                val bundle = Bundle()
-
-                val x = parent.getItemAtPosition(position)
-
-                Log.d("Helo", "itemposition $x")
                 Log.d("Helo", "Position: $position")
-                bundle.putString("title", articles[position].title)
-                bundle.putString("rating", articles[position].rating)
-                bundle.putString("content", articles[position].content)
-                bundle.putString("date", articles[position].date)
-                bundle.putString("author", articles[position].author)
-                bundle.putString("comments", articles[position].comments.toString())
+                val x = parent.getItemAtPosition(position)
+                Log.d("Helo", "currentItem: $x")
+
+                var newPos = -1
+                view as TextView
+                val text = view.text.toString()
+                Log.d("Helo", "text: $text")
+                for (i in 0 until articles.size) {
+                    if (articleTitles[i] == text)
+                        newPos = i
+                }
+
+                Log.d("Helo", "newpos: $newPos")
+
+                val bundle = Bundle()
+                bundle.putString("title", articles[newPos].title)
+                bundle.putString("rating", articles[newPos].rating)
+                bundle.putString("content", articles[newPos].content)
+                bundle.putString("date", articles[newPos].date)
+                bundle.putString("author", articles[newPos].author)
+                bundle.putString("comments", articles[newPos].comments.toString())
+
                 val fragment: Fragment = ArticleFragment()
                 fragment.arguments = bundle
                 fragmentManager?.beginTransaction()?.replace(R.id.navHostFragment, fragment)
