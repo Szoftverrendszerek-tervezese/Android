41ff4b9e926d10729756255b4e5bc98d95e00806
Horvath Aron <haronka99@gmail.com>
2020-12-06 13:54:59 +0200

    cleaning and some safety issue solved on searchfragment

--- app/src/main/java/com/example/application/home/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/application/home/fragments/SearchFragment.kt
@@ -1,22 +1,18 @@
 package com.example.application.home.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import com.example.application.R
 import com.example.application.databinding.FragmentSearchBinding
 import com.example.application.home.GeneralViewModel
-import com.example.application.home.adapters.RecyclerAdapter
 import com.example.application.home.models.RecyclerItem
 
 
@@ -26,12 +22,10 @@ class SearchFragment : Fragment() {
     private val viewModel: GeneralViewModel by activityViewModels()
     private lateinit var articles: MutableList<RecyclerItem>
     private var articleTitles: MutableList<String> = mutableListOf()
-    private var articlePairs: MutableList<Pair<String, String>> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         articles = viewModel.articles
-
     }
 
     override fun onCreateView(
@@ -40,15 +34,9 @@ class SearchFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         for (i in 0 until articles.size) {
-            var newString = articles[i].title + " by " + articles[i].author
-            val pair = Pair(articles[i].title, articles[i].author)
-            Log.d("Helo", "newstringre : $newString")
-            articlePairs.add(i, pair)
+            val newString = articles[i].title + " by " + articles[i].author
             articleTitles.plusAssign(newString)
         }
-
-        Log.d("Helo", "size: ${articles.size}")
-        Log.d("Helo", "articlePairs: $articlePairs")
 
         val adapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, articleTitles)
@@ -56,20 +44,20 @@ class SearchFragment : Fragment() {
 
         binding.autoComplete.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id -> //here the article need to be shown
-                Log.d("Helo", "Position: $position")
-                val x = parent.getItemAtPosition(position)
-                Log.d("Helo", "currentItem: $x")
-
                 var newPos = -1
                 view as TextView
                 val text = view.text.toString()
-                Log.d("Helo", "text: $text")
                 for (i in 0 until articles.size) {
-                    if (articleTitles[i] == text)
+                    if (articleTitles[i] == text) {
                         newPos = i
+                        break
+                    }
                 }
+                if (newPos == -1) {
 
-                Log.d("Helo", "newpos: $newPos")
+
+                    binding.autoComplete.error = "Somethign wrong"
+                }
 
                 val bundle = Bundle()
                 bundle.putString("title", articles[newPos].title)
